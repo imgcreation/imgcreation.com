9c55ee99941170c4a93e89ee06ab05c301e4e6c7
Hari om Bangari <hariom.bangari@outlook.com>
2025-03-13 07:05:30 +0000

    social media icon update

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -8,19 +8,19 @@ export const Footer = () => {
         <footer className="bg-white py-6">
         <div className="container mx-auto px-4">
           <div className="flex justify-center space-x-6 mb-4">
-            <Link href="https://instagram.com" className="text-gray-400 hover:text-teal-500">
+            <Link href="https://www.instagram.com/imgcreation_/" className="text-gray-400 hover:text-teal-500">
               <Instagram className="h-6 w-6" />
               <span className="sr-only">Instagram</span>
             </Link>
-            <Link href="https://youtube.com" className="text-gray-400 hover:text-teal-500">
+            <Link href="https://www.youtube.com/@IMGCRTN" className="text-gray-400 hover:text-teal-500">
               <Youtube className="h-6 w-6" />
               <span className="sr-only">YouTube</span>
             </Link>
-            <Link href="https://pinterest.com" className="text-gray-400 hover:text-teal-500">
+            <Link href="https://in.pinterest.com/imgcreation_" className="text-gray-400 hover:text-teal-500">
               <Pinterest className="h-6 w-6" />
               <span className="sr-only">Pinterest</span>
             </Link>
-            <Link href="https://facebook.com" className="text-gray-400 hover:text-teal-500">
+            <Link href="https://www.facebook.com/imgcreation" className="text-gray-400 hover:text-teal-500">
               <Facebook className="h-6 w-6" />
               <span className="sr-only">Facebook</span>
             </Link>
